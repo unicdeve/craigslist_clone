ed10d3aab116623527a615d940df98e5172f74eb
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-10-17 21:39:11 +0100

    search view, model url done

--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def search(request):
+    new_search = request.POST.get('search')
+    context = {
+        'search': new_search
+    }
+    return render(request, 'app/search.html', context)
+
